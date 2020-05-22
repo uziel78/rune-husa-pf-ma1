d8031567d5ea1793d63b1769d56547f1f81643ac
uziel78 <56655219+uziel78@users.noreply.github.com>
2020-05-22 12:46:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// question 1
+
+var x;
+
+// question 2
+
+var name = "rune";
+
+// question 3
+
+var y = 12;
+
+// question 4
+
+var division = 20 / 5;
+
+// question 5
+
+var z = "frog";
+typeof z;
+
+// question 6
+
+var orderHasShipped = true;
+
+// question 7
+
+if (orderHasShipped === true) {
+	console.log("true");
+} else {
+	console.log("false");
+}
+
+// question 8
+
+for (var i = 0; i <= 9; i++) {
+	console.log(i);
+}
